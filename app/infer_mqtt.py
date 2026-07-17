85c66ca6cb24594ddf2871d9f59015b6b7b778f8
Truong Long Khanh <85059988+khanhlong247@users.noreply.github.com>
2026-07-18 06:06:26 +0700

    Change EfficientNet model to not use pretrained weights

--- app/infer_mqtt.py
+++ app/infer_mqtt.py
@@ -238,7 +238,7 @@ mqtt_thread.start()
 class SimpleCardClassifier(nn.Module):
     def __init__(self, num_classes=53):
         super(SimpleCardClassifier, self).__init__()
-        self.base_model = timm.create_model('efficientnet_b0', pretrained=True)
+        self.base_model = timm.create_model('efficientnet_b0', pretrained=False)
         self.features = nn.Sequential(*list(self.base_model.children())[:-1])
         enet_out_size = 1280
         self.classifier = nn.Sequential(
